f84687a861d0749096bb9bbb60cbf08974515c21
Wellisson Nunes <wellissonln@gmail.com>
2021-03-31 22:07:54 -0300

    crud ok, with password encrypt on create user

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import databases, sqlalchemy, asyncpg
+import databases, sqlalchemy, asyncpg, datetime, uuid
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from typing import List
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 #db
@@ -37,7 +40,20 @@ class UserList(BaseModel):
     gender: str
     created_at: str
     status: str
-
+class UserEntry(BaseModel):
+    username: str = Field(..., example="user")
+    password: str = Field(..., example="password")
+    first_name: str = Field(..., example="Well")
+    last_name: str = Field(..., example="Nunes")
+    gender: str = Field(..., example="M")
+class UserUpdate(BaseModel):
+    id: str = Field(..., example="enter id here")
+    first_name: str = Field(..., example="Well")
+    last_name: str = Field(..., example="Nunes")
+    gender: str = Field(..., example="M")
+    status: str = Field(..., example="1")
+class UserDelete(BaseModel):
+    id: str = Field(..., example="enter id here")
 
 
 app = FastAPI()
@@ -54,3 +70,56 @@ async def shutdown():
 async def find_all_users():
     query = users.select()
     return await database.fetch_all(query)
+
+@app.post("/users", response_model=UserList)
+async def register_user(user: UserEntry):
+    gID = str(uuid.uuid1())
+    gDate = str(datetime.datetime.now())
+    query = users.insert().values(
+        id = gID,
+        username = user.username,
+        password = pwd_context.hash(user.password),
+        first_name = user.first_name,
+        last_name = user.last_name,
+        gender = user.gender,
+        created_at = gDate,
+        status = "1"
+    )
+
+    await database.execute(query)
+    return {
+        "id": gID,
+        **user.dict(),
+        "created_at":gDate,
+        "status": "1"
+    }
+
+@app.get("/users/{userId}", response_model=UserList)
+async def find_user_by_id(userId: str):
+    query = users.select().where(users.c.id == userId)
+    return await database.fetch_one(query)
+
+@app.put("/users", response_model=UserList)
+async def update_user(user: UserUpdate):
+    gDate = str(datetime.datetime.now())
+    query = users.update().\
+        where(users.c.id == user.id).\
+        values(
+            first_name = user.first_name,
+            last_name = user.last_name,
+            gender = user.gender,
+            created_at = gDate,
+        )
+    await database.execute(query)
+
+    return await find_user_by_id(user.id)
+
+@app.delete("/users/{userId}")
+async def delete_user(user:UserDelete):
+    query = users.delete().where(users.c.id == user.id)
+    await database.execute(query)
+
+    return {
+        "status": True,
+        "message": "User deleted."
+    }
